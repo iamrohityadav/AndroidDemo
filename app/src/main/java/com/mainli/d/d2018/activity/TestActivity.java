b93a1a648523284c51d92d19b51ae2d081da34c7
lixiaoliang <lixiaoliang@mobimagic.com>
2018-03-21 11:34:02 +0800

    增加TestActivity用于Debug Rxjava开元库以及阅读源码

---
+++ app/src/main/java/com/mainli/d/d2018/activity/TestActivity.java
@@ -0,0 +1,47 @@
+package com.mainli.d.d2018.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by lixiaoliang on 2018-3-21.
+ */
+
+public class TestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        rx.Observable.create(new rx.Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                subscriber.onNext(66666);
+            }
+        }).map(new Func1<Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer) {
+                return 2222;
+            }
+        }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+
+            }
+        });
+    }
+}
