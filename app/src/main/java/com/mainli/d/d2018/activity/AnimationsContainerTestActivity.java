f65a2cd29efd4431b292dcb628cbc449e8a301fb
lixiaoliang <lixiaoliang@mobimagic.com>
2018-03-23 17:02:04 +0800

    增加引入了BitmapFactory.Options.inBitmap字段注意事项

--- app/src/main/java/com/mainli/d/d2018/activity/AnimationsContainerTestActivity.java
+++ app/src/main/java/com/mainli/d/d2018/activity/AnimationsContainerTestActivity.java
@@ -16,6 +16,19 @@ import com.mainli.d.d2018.animutils.AnimationsContainer;
  * TITLE
  * Created by shixiaoming on 16/12/27.
  * 来源: https://github.com/VDshixiaoming/AnimationTest
+ *
+ * Android3.0(API 11之后)引入了BitmapFactory.Options.inBitmap字段，设置此字段之后解码方法会尝试复用一张存在的Bitmap。
+ * 这意味着Bitmap的内存被复用，避免了内存的回收及申请过程，显然性能表现更佳。不过，使用这个字段有几点
+ * 声明可被复用的Bitmap必须设置inMutable为true；
+ * Android4.4(API 19)之前只有格式为jpg、png，同等宽高（要求苛刻），inSampleSize为1的Bitmap才可以复用；
+ * Android4.4(API 19)之前被复用的Bitmap的inPreferredConfig会覆盖待分配内存的Bitmap设置的inPreferredConfig；
+ * Android4.4(API 19)之后被复用的Bitmap的内存必须大于需要申请内存的Bitmap的内存；
+ * Android4.4(API 19)之前待加载Bitmap的Options.inSampleSize必须明确指定为1。
+ * <p>
+ * 作者：双十二技术哥
+ * 链接：https://www.jianshu.com/p/e49ec7d053b3
+ * 來源：简书
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
 public class AnimationsContainerTestActivity extends AppCompatActivity {
     private ImageView imageView;
